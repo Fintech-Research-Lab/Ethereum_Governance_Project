ae82d17a95b2e4e196e7d214fae8a8f3f0d44dd4
Moazzam Khoja <89045782+moazzamkhoja@users.noreply.github.com>
2023-06-27 12:21:34 -0500

    Update and rename Mean Sentiment Scores.R to Create Sentiment Scores.R

--- Sentiment Analysis/Create Sentiment Scores.R
+++ Sentiment Analysis/Create Sentiment Scores.R
@@ -7,9 +7,6 @@
 # This code was modified on 5-17-2023. The results are not imported in stata file correctly. I will modify columns to reduce columns that are not needed
 # for regressions and merge them into a stata data. In addition, I also want to add company names in the database
 
-# on 5-18-2023 this code was modified to include author1 - author6 fields in the depracated code because we need this in stata to create dummies for
-# ethereum foundation and Vitalik, I also added author7 which was missed in the previous code
-
 library(data.table)
 library(tidyr)
 library(dplyr)
@@ -17,6 +14,7 @@ library(stringr)
 library(DescTools)
 library(writexl)
 library(jsonlite)
+library(haven)
 
 d <- fread("C:/Users/moazz/Box/Fintech Research Lab/Ethereum Governance Project/Old Data WIP/magician_comments_separated_withresults.csv")
 # convert a list of list into labels and scores list
@@ -25,15 +23,15 @@ d$scores <-  str_extract_all(d$score, "(?<=score': )[0-9]\\.[0-9]+")
 
 # create filtered scores
 for(i in 1:nrow(d)){
-  d$filtered_score[i] <- lapply(d$scores[i],function(x){ifelse(as.numeric(x)>0.5,d$labels[i],character(0))})
+d$filtered_score[i] <- lapply(d$scores[i],function(x){ifelse(as.numeric(x)>0.5,d$labels[i],character(0))})
 }
 # remove NAs from filtered score
 for(i in 1:nrow(d)){
-  d$filtered_score_notna[i] <- sapply(d$filtered_score[i], function(x){Filter(Negate(is.na), x)})
+d$filtered_score_notna[i] <- sapply(d$filtered_score[i], function(x){Filter(Negate(is.na), x)})
 }
 # calculate mode sentiment score
 for(i in 1:nrow(d)){
-  d$mode_sentiment[i] <- Mode(as.numeric(unlist(d$filtered_score_notna[i])))
+d$mode_sentiment[i] <- Mode(as.numeric(unlist(d$filtered_score_notna[i])))
 }
 
 for(i in 1:nrow(d)){
@@ -49,3 +47,32 @@ summary(d$mean_sentiment)
 d1 <- d %>% select(EIP_Number,Title,Author,mode_sentiment,mean_sentiment)
 
 fwrite(d1,"C:/Users/moazz/Box/Fintech Research Lab/Ethereum Governance Project/data_with_mode_sentiment_reduced.csv")
+
+# to write the code in csv I will need to convert list into characters
+
+d$score <- lapply(d$score, toJSON, auto_unbox = TRUE)
+d$labels <- sapply(d$labels, paste, collapse = ",")
+d$scores <- sapply(d$scores, paste, collapse = ",")
+d$filtered_score <- sapply(d$filtered_score, paste, collapse = ",")
+d$filtered_score_notna <- sapply(d$filtered_score_notna, paste, collapse = ",")
+
+fwrite(d,"C:/Users/moazz/Box/Fintech Research Lab/Ethereum Governance Project/sentiment_data.csv")
+
+# create a depracated version of the file
+
+d <- rename(d,Index = V1)
+
+d_dep <- d %>% dplyr::select(EIP_Number, Title, Website, Author,mod_comment,mode_sentiment,mean_sentiment)
+
+fwrite(d_dep, "C:/Users/moazz/Box/Fintech Research Lab/Ethereum Governance Project/data_with_mode_sentiment.csv" )
+
+
+
+
+
+
+
+
+
+
+
