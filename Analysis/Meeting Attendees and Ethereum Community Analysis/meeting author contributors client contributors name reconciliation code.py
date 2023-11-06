cfc6d2efef2e35bfbee43541822cfb39fe98d3f0
Moazzam Khoja <moazzam.khoja@gmail.com>
2023-11-06 10:40:51 -0600

    Update meeting author contributors client contributors name reconciliation code.py

--- Analysis/Meeting Attendees and Ethereum Community Analysis/meeting author contributors client contributors name reconciliation code.py
+++ Analysis/Meeting Attendees and Ethereum Community Analysis/meeting author contributors client contributors name reconciliation code.py
@@ -13,12 +13,12 @@ import numpy as np
 
 # Read Data
 
-os.chdir("C:/Users/khojama/Box/Fintech Research Lab/Ethereum_Governance_Project/Analysis/Meeting Attendees and Ethereum Community Analysis/")
+os.chdir("C:/Users/moazz/Box/Fintech Research Lab/Ethereum_Governance_Project/Analysis/Meeting Attendees and Ethereum Community Analysis/")
 attendees = pd.read_csv("flat_list_meeting_attendees.csv", encoding='latin1')
 
 
 # get author data and remove nan and et al. from the author list
-os.chdir ("C:/Users/khojama/Box/Fintech Research Lab/Ethereum_Governance_Project/Data/Raw Data/")
+os.chdir ("C:/Users/moazz/Box/Fintech Research Lab/Ethereum_Governance_Project/Data/Raw Data/")
 cs = pd.read_csv("Ethereum_Cross-sectional_Data.csv")
 author_values = cs.loc[:,'Author1':'Author15'].values.tolist()
 author = set([item for sublist in author_values for item in sublist])
@@ -29,7 +29,7 @@ author = author[pd.notnull(author['full_name']) & (author['full_name'] != 'et al
 eip_commit = pd.read_stata("eip_Commit.dta")
 
 # get client data
-os.chdir("C:/Users/khojama/Box/Fintech Research Lab/Ethereum_Governance_Project/Data/Commit Data/client_commit/")
+os.chdir("C:/Users/moazz/Box/Fintech Research Lab/Ethereum_Governance_Project/Data/Commit Data/client_commit/")
 geth = pd.read_stata("commitsgeth.dta")
 besu = pd.read_stata("commitsbesu.dta")
 erigon = pd.read_stata("commitserigon.dta")
@@ -78,7 +78,7 @@ name_to_replace_with = [row[2] for row in high_similarity_score1]
 # manually change the list. This process is better done in a spreadsheet manually
 
 names_to_change = pd.concat([pd.DataFrame(name_to_replace_with),pd.DataFrame(name_to_replace)], axis = 1)
-os.chdir("C:/Users/khojama/Box/Fintech Research Lab/Ethereum_Governance_Project/Analysis/Meeting Attendees and Ethereum Community Analysis/")
+os.chdir("C:/Users/moazz/Box/Fintech Research Lab/Ethereum_Governance_Project/Analysis/Meeting Attendees and Ethereum Community Analysis/")
 names_to_change.to_csv("names_to_change.csv")
 
 # There was a manual process to map names to replace and names_to_replace_with
@@ -125,6 +125,17 @@ names4 = [name_dict3[name] if name in name_dict3 else name for name in names3]
 
 unique_attendee3 = pd.DataFrame(pd.Series(names4).unique(), columns = ['full_name'])
 unique_attendee4 = unique_attendee3[(pd.notnull(unique_attendee3['full_name'])) & (unique_attendee3['full_name'] != "NONE")]
+
+
+# create mapping file
+df1 = pd.DataFrame(names, columns=['Original_Name'])
+df2 = pd.DataFrame(names4, columns=['Replace_Name'])
+
+mapping_file = pd.concat([df1, df2], axis=1)
+
+# save mapping file
+
+mapping_file.to_csv("Mapping_File.csv")
 
 
 # Now the code will do cross-list check of names with Contributors Client, and Author Lists
