603096667163ad7389cb7788dbf48aee6309f7ef
Moazzam Khoja <moazzam.khoja@gmail.com>
2023-11-17 11:49:54 -0600

    Update meeting dates volatility python code.py

--- Analysis/Meeting Dates Volatility Analysis/meeting dates volatility python code.py
+++ Analysis/Meeting Dates Volatility Analysis/meeting dates volatility python code.py
@@ -10,25 +10,26 @@ import os as os
 import numpy as np
 
 # get meeting dates
-os.chdir("C:/Users/khojama/Box/Fintech Research Lab/Ethereum_Governance_Project/Analysis/Meeting Dates Volatility Analysis/")
+os.chdir("C:/Users/moazz/Box/Fintech Research Lab/Ethereum_Governance_Project/Analysis/Meeting Dates Volatility Analysis/")
 meeting = pd.read_csv("calls_updated_standardized.csv")
-meeting_dates = meeting['Date']
+meeting_dates = pd.DataFrame(meeting['Date'], columns = ['Date'])
 meeting_dates['Date'].replace('Missing', pd.NaT, inplace=True)
-eth['Meeting_Dates'] = pd.to_datetime(eth['']).dt.strftime('%Y-%m-%d')
+meeting_dates['Date'] = pd.to_datetime(meeting_dates['Date']).dt.strftime('%Y-%m-%d')
 
 
 # get Eth prices and generate daily hourly volatility
-os.chdir ("C:/Users/khojama/Box/Fintech Research Lab/Ethereum_Governance_Project/Analysis/Abnormal Returns/")
+os.chdir ("C:/Users/moazz/Box/Fintech Research Lab/Ethereum_Governance_Project/Analysis/Abnormal Returns/")
 eth_prices = pd.read_csv("eth_prices.csv")
 eth_prices['START'] = pd.to_datetime(eth_prices['START']).dt.tz_convert('US/Eastern')
 eth_vols = eth_prices.groupby('START_DATE')['CLOSE'].std()
 eth_vols = pd.DataFrame(eth_vols)
 eth_vols['Date'] = eth_vols.index
+eth_vols['Date'] = pd.to_datetime(eth_vols['Date']).dt.strftime('%Y-%m-%d')
+eth_vols = eth_vols.rename(columns = {'CLOSE':'Intra_day_vol'})
 
+eth_vols['Meeting'] = np.where(eth_vols['Date'].isin(meeting_dates['Date']),1,0)
 
-eth = pd.merge(eth_vols,meeting_dates, left_on = "START_DATE", right_on = 'Date', how = 'outer', indicator = True)
-eth = eth.rename(columns = {'Date_x' : 'Date','Date_y':'Meeting_Dates', 'CLOSE':'Hourly_vol'})
-eth['Date'] = pd.to_datetime(eth['Date']).dt.strftime('%Y-%m-%d')
+indices = np.where(~meeting_dates['Date'].isin(eth_vols['Date']))[0]
+meetings_not_covered = meeting_dates.loc[indices,'Date']
 
-
-eth = eth.sort_values(["Date","Meeting_Dates"])
+eth_vols.to_csv("eth_vols.csv")
