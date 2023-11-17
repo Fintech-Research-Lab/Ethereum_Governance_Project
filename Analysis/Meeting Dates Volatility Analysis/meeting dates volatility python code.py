66ae7e2d5c9d0c042d5295a41f83393f981e8f93
Moazzam Khoja <moazzam.khoja@gmail.com>
2023-11-17 16:43:37 -0600

    Update meeting dates volatility python code.py

--- Analysis/Meeting Dates Volatility Analysis/meeting dates volatility python code.py
+++ Analysis/Meeting Dates Volatility Analysis/meeting dates volatility python code.py
@@ -10,18 +10,20 @@ import os as os
 import numpy as np
 
 # get meeting dates
-os.chdir("C:/Users/moazz/Box/Fintech Research Lab/Ethereum_Governance_Project/Analysis/Meeting Dates Volatility Analysis/")
-meeting = pd.read_csv("calls_updated_standardized.csv")
+os.chdir("C:/Users/moazz/Box/Fintech Research Lab/Ethereum_Governance_Project/")
+meeting = pd.read_csv("/Analysis/Meeting Dates Volatility Analysis/calls_updated_standardized.csv")
 meeting_dates = pd.DataFrame(meeting['Date'], columns = ['Date'])
 meeting_dates['Date'].replace('Missing', pd.NaT, inplace=True)
 meeting_dates['Date'] = pd.to_datetime(meeting_dates['Date']).dt.strftime('%Y-%m-%d')
 
 
 # get Eth prices and generate daily hourly volatility
-os.chdir ("C:/Users/moazz/Box/Fintech Research Lab/Ethereum_Governance_Project/Analysis/Abnormal Returns/")
+os.chdir ("Data/Raw Data/")
 eth_prices = pd.read_csv("eth_prices.csv")
 eth_prices['START'] = pd.to_datetime(eth_prices['START']).dt.tz_convert('US/Eastern')
-eth_vols = eth_prices.groupby('START_DATE')['CLOSE'].std()
+eth_prices = eth_prices.sort_values('START')
+eth_prices['return'] = eth_prices['CLOSE'].pct_change()
+eth_vols = eth_prices.groupby('START_DATE')['return'].std()
 eth_vols = pd.DataFrame(eth_vols)
 eth_vols['Date'] = eth_vols.index
 eth_vols['Date'] = pd.to_datetime(eth_vols['Date']).dt.strftime('%Y-%m-%d')
