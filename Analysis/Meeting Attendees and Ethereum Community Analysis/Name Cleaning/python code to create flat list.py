42f929afaddea3e32b2023117c4541d4d8ca816f
Moazzam Khoja <moazzam.khoja@gmail.com>
2023-12-15 12:45:52 -0600

    Create python code to create flat list.py

---
+++ Analysis/Meeting Attendees and Ethereum Community Analysis/Name Cleaning/python code to create flat list.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 30 02:08:12 2023
+
+@author: moazz
+"""
+
+import pandas as pd
+import os as os
+import re
+
+# Read Data
+os.chdir("C:/Users/khojama/Box/Fintech Research Lab/Ethereum_Governance_Project/Analysis/Meeting Attendees and Ethereum Community Analysis/")
+dat = pd.read_csv("dev_calls_attendees_standardized_v2.csv", encoding='latin1')
+
+
+# Function to clean text
+def clean_text(text):
+    text = re.sub(r'\(.*?\)', '', text)  # Remove everything within ()
+    text = re.sub(r'<.*?>', '', text)  # Remove everything within <>
+    text = text.strip()  # Remove leading and trailing spaces
+    return None if text == '' else text  # Return None for empty strings
+
+# Apply the function to each name in the 'names' column
+dat['names'] = dat['Attendees'].str.split(",").apply(lambda x: [i for i in [clean_text(j) for j in x] if i is not None])
+names = pd.DataFrame(dat['names'])
+
+
+# create flt list for names
+flat_list = [name for sublist in dat['names'] for name in sublist]
+flat_names = pd.DataFrame(flat_list)
+
+# create flat list for meetings
+meeting_list = []
+for index, row in dat.iterrows():
+    meetings = [row['Meeting']] * len(row['names'])
+    meeting_list.extend(meetings)   
+meetings = pd.DataFrame(meeting_list)
+meetings = meetings.rename(columns = {0:'Meeting'})
+
+# combine flat meetings and attendees to create a new list
+new_list = pd.concat([meetings, flat_names], axis = 1)
+new_list = new_list.rename(columns = {0:"Attendees"})
+
+new_list.to_csv("flat_list_meeting_attendees.csv")
+
